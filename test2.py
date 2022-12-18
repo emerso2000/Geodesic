5cd391c65827aaab444e23cbbdaed46c8e06ca73
emerso2000 <83967836+emerso2000@users.noreply.github.com>
2022-12-18 15:34:52 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -47,34 +47,8 @@ class Schwarzschild:
         christoffel_symbols = np.array(christoffel_symbols)
         christoffel_symbols = np.reshape(christoffel_symbols, (4, 4, 4))
 
-        print(christoffel_symbols)
+        # print(christoffel_symbols)
         return christoffel_symbols
-    
-    # def calc_christoffel(self):
-    #     """Calculate the Christoffel symbols for the Schwarzschild metric."""
-    #
-    #     coord_list = [self.t, self.r, self.theta, self.phi]
-    #
-    #     christoffel_symbols = []
-    #
-    #     for i in range(4):
-    #         for j in range(4):
-    #             for k in range(4):
-    #                 term1 = 0.5 * (sym.diff(self.g[i, j], coord_list[k]) +
-    #                                sym.diff(self.g[i, k], coord_list[j]) -
-    #                                sym.diff(self.g[j, k], coord_list[i]))
-    #                 term2 = 0
-    #                 for m in range(4):
-    #                     term2 += 0.5 * self.g[i, m] * (sym.diff(self.g[m, j], coord_list[k]) +
-    #                                                    sym.diff(self.g[m, k], coord_list[j]) -
-    #                                                    sym.diff(self.g[j, k], coord_list[m]))
-    #                 christoffel_symbols.append(term1 + term2)
-    #
-    #     christoffel_symbols = np.array(christoffel_symbols)
-    #     christoffel_symbols = np.reshape(christoffel_symbols, (4, 4, 4))
-    #
-    #     print(christoffel_symbols)
-    #     return christoffel_symbols
 
     def calc_geodesic_equations(self):
         """Calculate the geodesic equations for the Schwarzschild metric."""
@@ -96,27 +70,52 @@ class Schwarzschild:
         # print(geodesic_equations)
         return geodesic_equations
 
+    # def calc_geodesic(self, initial_condition, step_size, num_steps):
+    #     # coord_array = np.array([self.t, self.r, self.theta, self.phi])
+    #     geodesic_equations = self.calc_geodesic_equations()
+    #
+    #     trajectory = sym.zeros(num_steps + 1, 4)
+    #
+    #     # Set the initial condition
+    #     trajectory[0] = initial_condition
+    #     for i in range(num_steps):
+    #         # Check if the value of r has become equal to or less than the Schwarzschild radius
+    #         # print(trajectory)
+    #         if trajectory[0] <= self.r_s:
+    #             print("Geodesic terminated at step", i, "due to r <= r_s")
+    #             print(trajectory[i, 1])
+    #             print(self.r_s)
+    #             print(bool(trajectory[i, 1] <= self.r_s))
+    #             break
+    #         for j in range(4):
+    #             trajectory[i + 1] = trajectory[i, j] + step_size * geodesic_equations[j].subs(
+    #                 [(self.t, trajectory[i, 0]), (self.r, trajectory[i, 1]),
+    #                  (self.theta, trajectory[i, 2]), (self.phi, trajectory[i, 3])])
+    #
+    #     #print(trajectory)
+    #     return trajectory
     def calc_geodesic(self, initial_condition, step_size, num_steps):
-        # coord_array = np.array([self.t, self.r, self.theta, self.phi])
+        """Calculate the trajectory of a geodesic using Euler's method."""
+        # Get the geodesic equations
         geodesic_equations = self.calc_geodesic_equations()
 
+        # Initialize the trajectory array with the initial condition
         trajectory = sym.zeros(num_steps + 1, 4)
-
-        # Set the initial condition
         trajectory[0] = initial_condition
 
+        # Calculate the trajectory
         for i in range(num_steps):
             for j in range(4):
                 trajectory[i + 1, j] = trajectory[i, j] + step_size * geodesic_equations[j].subs(
-                    [(self.t, trajectory[i, 0]), (self.r, trajectory[i, 1]), (self.theta, trajectory[i, 2]),
-                     (self.phi, trajectory[i, 3])])
+                    [(self.t, trajectory[i, 0]), (self.r, trajectory[i, 1]),
+                     (self.theta, trajectory[i, 2]), (self.phi, trajectory[i, 3])])
 
-        # print(trajectory)
+        print(trajectory)
         return trajectory
 
 
-schwarzschild = Schwarzschild(1, 6.67408e-11, 1.989e12)
-# schwarzschild.print_metric()
+initial_condition = sym.Matrix([0, 10, 0, 0])
+schwarzschild = Schwarzschild(2, 6.67408e-11, 1.989e12)
 schwarzschild.calc_christoffel()
-# schwarzschild.calc_geodesic_equations()
-# schwarzschild.calc_geodesic([120, 12, 4, 7], 0.1, 1000)
+schwarzschild.calc_geodesic_equations()
+schwarzschild.calc_geodesic(initial_condition, 0.1, 1000)
