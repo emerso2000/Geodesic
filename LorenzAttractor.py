0137918461fa299202b70f3f9d62ea34945925a9
emerso2000 <83967836+emerso2000@users.noreply.github.com>
2023-01-24 08:06:46 -0500

    Update LorenzAttractor.py

--- LorenzAttractor.py
+++ LorenzAttractor.py
@@ -19,33 +19,55 @@ counter_initial = 0
 
 class LorenzAttractor:
     def __init__(self):
-        self.x = 0
-        self.y = 0.02
-        self.z = 0.01
-        
-        self.angle = angle_initial
-        self.counter = counter_initial
+        self.x = 0.01
+        self.y = 0
+        self.z = 0
+
+        self.angle = 0.01
+        self.counter = 0
         self.points = []
-        
+
         self.a = 10.0
         self.b = 28.0
         self.c = 8.0 / 3.0
+
+        self.display = (720, 720)
 
     def lorenz_equations(self):
         dx = 0
         dy = 0
         dz = 0
 
-        # glVertex3d(self.x, self.y, self.z)
+        dt = 0.01
 
-        self.x = self.x + dx
-        self.y = self.y + dy
-        self.z = self.z + dz
+        while self.counter < 250:
+            glColor3d(1, 1, 1)
+            dx = self.a * (self.y - self.x) * dt
+            dy = (self.x * (self.b - self.z) - self.y) * dt
+            dz = (self.x * self.y - self.c * self.z) * dt
 
-        self.counter += 1
+            glVertex3d(self.x, self.y, self.z)
 
-        self.points.append((self.x, self.y, self.z))
-        
+            self.x += dx
+            self.y += dy
+            self.z += dz
+
+            self.counter += 1
+
+            self.points.append((self.x, self.y, self.z))
+
+        return self.points
+
 
 integrator = LorenzAttractor()
-print(integrator.points)
+points = integrator.lorenz_equations()
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = fig.add_subplot(projection="3d")
+plt.title("Lorenz Attractor")
+for x, y, z in points:
+    ax.scatter3D(x, y, z)
+plt.show()
