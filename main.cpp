941e09f3f047ef506dac0840867b2b366cab4fb8
emerso2000 <83967836+emerso2000@users.noreply.github.com>
2023-04-11 17:24:23 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,272 +1,199 @@
+#include <random>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include "../header_files/shader_s.h"
 #include <iostream>
-#include <iostream>
-#include <vector>
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include <fstream>
 #include <glm/glm.hpp>
-// for transformations:
-#include <glm/gtc/matrix_transform.hpp>
-// to pass matrices to the shader
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/random.hpp>
+#include <ctime>
+#include <cstdlib>
+#include <cmath>
 
-GLFWwindow *initialize(int width, int height)
+/**
+ * The number of particles we want to simulate. Since compute shaders are limited in their
+ * work group size, we'll also need to know the work group size so we can find out how many
+ * work groups to dispatch
+ */
+#define NUM_PARTICLES 100
+
+// This MUST match with local_size_x inside cursor.glsl
+#define WORK_GROUP_SIZE 10
+
+#define SCREENX 1920
+#define SCREENY 1080
+
+GLchar *LoadShader(const std::string &file)
 {
+	std::ifstream shaderFile;
+	long shaderFileLength;
+
+	shaderFile.open(file);
+
+	if (shaderFile.fail())
+	{
+		throw std::runtime_error("COULD NOT FIND SHADER FILE");
+	}
+
+	shaderFile.seekg(0, shaderFile.end);
+	shaderFileLength = shaderFile.tellg();
+	shaderFile.seekg(0, shaderFile.beg);
+
+	GLchar *shaderCode = new GLchar[shaderFileLength + 1];
+	shaderFile.read(shaderCode, shaderFileLength);
+
+	shaderFile.close();
+
+	shaderCode[shaderFileLength] = '\0';
+
+	return shaderCode;
+}
+
+int main(int argc, char **argv)
+{
+	srand(time(0));
+
+	const GLfloat delta_time = 0.01f;
+
+	// Window Setup
 	glfwInit();
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow *window = glfwCreateWindow(640, 480, "This is working I hope", NULL, NULL);
-	if (!window)
-	{
-		std::cout << "Window creation failed\n";
-		return NULL;
-	}
+	GLFWwindow *window = glfwCreateWindow(SCREENX, SCREENY, "Particles", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
 
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	gladLoadGL();
+	
+	// Setup Initial Positions/Velocities
+	glm::vec3 positions[NUM_PARTICLES];
+	glm::vec3 velocities[NUM_PARTICLES];
+
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<double> dis1(0, M_PI);
+	std::uniform_real_distribution<double> dis2(0, M_PI);
+	std::uniform_real_distribution<double> dis3(1, 10);
+	std::uniform_int_distribution<int> dis4(1, 10);
+
+	for (int i = 0; i < NUM_PARTICLES; i++)
 	{
-		std::cout << "GLAD initialization failed\n";
-		return NULL;
+		double theta = dis1(gen);
+		double phi = dis2(gen);
+		double rand_mass = dis3(gen);
+		int r = dis4(gen);
+
+		float velx = r * sin(theta) * cos(phi);
+		float vely = r * sin(theta) * sin(phi);
+		float velz = r * cos(theta);
+
+		positions[i] = glm::vec3(velx, vely, velz);
+		velocities[i] = glm::vec3(0.0f, 0.0f, 0.0f);
 	}
 
-	glViewport(0, 0, 640, 480);
 
-	return window;
-}
+	GLuint vao, pos, timebuffer, vel;
 
-void processInput(GLFWwindow *window, glm::vec3 &playerPosition, glm::vec3 &playerEulers)
-{
+	glCreateVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 
-	int wasdState{0};
-	float walk_direction{playerEulers.z};
-	bool walking{false};
+	glCreateBuffers(1, &pos);
+	glCreateBuffers(1, &timebuffer);
+	glCreateBuffers(1, &vel);
 
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-	{
-		wasdState += 1;
-	}
+	glNamedBufferData(vel, sizeof(velocities), velocities, GL_STATIC_DRAW);
+	glNamedBufferData(pos, sizeof(positions), positions, GL_STATIC_DRAW);
+	glNamedBufferData(timebuffer, sizeof(GLfloat), &delta_time, GL_DYNAMIC_DRAW);
 
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-	{
-		wasdState += 2;
-	}
+	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, pos);
+	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, vel);
+	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, timebuffer);
+	/*
+	 * Basic Shader Setup - This is standard shader loading and compilation.
+	 */
+	const GLchar *vertCode = LoadShader("../shader_files/shader.vert");
+	const GLchar *fragCode = LoadShader("../shader_files/shader.frag");
+	const GLchar *computeCode = LoadShader("../shader_files/NBodies.glsl");
 
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-	{
-		wasdState += 4;
-	}
+	GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+	GLuint computeShader = glCreateShader(GL_COMPUTE_SHADER);
 
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-	{
-		wasdState += 8;
-	}
+	glShaderSource(vertShader, 1, &vertCode, nullptr);
+	glShaderSource(fragShader, 1, &fragCode, nullptr);
+	glShaderSource(computeShader, 1, &computeCode, nullptr);
 
-	switch (wasdState)
-	{
-	case 1:
-	case 11:
-		// forwards
-		walking = true;
-		break;
-	case 3:
-		// left-forwards
-		walking = true;
-		walk_direction += 45;
-		break;
-	case 2:
-	case 7:
-		// left
-		walking = true;
-		walk_direction += 90;
-		break;
-	case 6:
-		// left-backwards
-		walking = true;
-		walk_direction += 135;
-		break;
-	case 4:
-	case 14:
-		// backwards
-		walking = true;
-		walk_direction += 180;
-		break;
-	case 12:
-		// right-backwards
-		walking = true;
-		walk_direction += 225;
-		break;
-	case 8:
-	case 13:
-		// right
-		walking = true;
-		walk_direction += 270;
-		break;
-	case 9:
-		// right-forwards
-		walking = true;
-		walk_direction += 315;
-	}
+	GLchar infolog[512];
 
-	if (walking)
-	{
-		playerPosition += 0.1f * glm::vec3{
-									 glm::cos(glm::radians(walk_direction)),
-									 glm::sin(glm::radians(walk_direction)),
-									 0.0f};
-	}
+	glCompileShader(vertShader);
+	glGetShaderInfoLog(vertShader, 512, nullptr, infolog);
+	std::cout << infolog << std::endl;
 
-	double mouse_x, mouse_y;
-	glfwGetCursorPos(window, &mouse_x, &mouse_y);
-	glfwSetCursorPos(window, static_cast<double>(640 / 2), static_cast<double>(480 / 2));
+	glCompileShader(fragShader);
+	glGetShaderInfoLog(fragShader, 512, nullptr, infolog);
+	std::cout << infolog << std::endl;
 
-	float delta_x{static_cast<float>(mouse_x - static_cast<double>(640 / 2))};
-	playerEulers.z -= delta_x;
+	glCompileShader(computeShader);
+	glGetShaderInfoLog(computeShader, 512, nullptr, infolog);
+	std::cout << infolog << std::endl;
 
-	float delta_y{static_cast<float>(mouse_y - static_cast<double>(480 / 2))};
-	playerEulers.y = std::max(std::min(playerEulers.y + delta_y, 180.0f), 0.0f);
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertShader);
+	glAttachShader(shaderProgram, fragShader);
+	glLinkProgram(shaderProgram);
+	glDeleteShader(vertShader);
+	glDeleteShader(fragShader);
 
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-	{
-		glfwSetWindowShouldClose(window, true);
-	}
-}
+	glGetProgramInfoLog(shaderProgram, 512, nullptr, infolog);
 
+	std::cout << infolog << std::endl;
 
-int main()
-{
-	glm::vec3 playerPosition = {0.0f, 0.0f, 1.0f};
-	glm::vec3 playerEulers = {0.0f, 90.0f, 0.0f};
+	GLuint computeProgram = glCreateProgram();
+	glAttachShader(computeProgram, computeShader);
+	glLinkProgram(computeProgram);
+	glDeleteShader(computeShader);
 
-	int width = 640;
-	int height = 480;
+	glGetProgramInfoLog(computeProgram, 512, nullptr, infolog);
+	std::cout << infolog << std::endl;
 
-	float aspectRatio = (float)width / float(height);
-
-	GLFWwindow *window = initialize(width, height);
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
-
-    // build and compile our shader program
-    Shader ourShader("../shader_files/shader.vs", "../shader_files/shader.fs");
-	ourShader.use();
-
-	std::vector<float> vertices = {{-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, // bottom
-									0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
-									0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-
-									0.5f, 0.5f, -0.5f, 1.0f, 1,
-									-0.5f, 0.5f, -0.5f, 0.0f, 1,
-									-0.5f, -0.5f, -0.5f, 0.0f, 0,
-
-									-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, // top
-									0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-									0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
-
-									0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
-									-0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
-									-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-
-									-0.5f, 0.5f, 0.5f, 1.0f, 0.0f, // left
-									-0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-									-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-
-									-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-									-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-									-0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-
-									0.5f, 0.5f, 0.5f, 1.0f, 0.0f, // right
-									0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-									0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-
-									0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-									0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-									0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-
-									-0.5f, -0.5f, -0.5f, 0.0f, 1.0f, // back
-									0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
-									0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-
-									0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-									-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-									-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-
-									-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, // front
-									0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-									0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-
-									0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-									-0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
-									-0.5f, 0.5f, -0.5f, 0.0f, 1.0f}};
-
-	int vertexCount = vertices.size() / 5;
-	unsigned int VBO;
-	glCreateBuffers(1, &VBO);
-	unsigned int VAO;
-	glCreateVertexArrays(1, &VAO);
-	glVertexArrayVertexBuffer(VAO, 0, VBO, 0, 5 * sizeof(float));
-	glNamedBufferStorage(VBO, vertices.size() * sizeof(float), vertices.data(), GL_DYNAMIC_STORAGE_BIT);
-	glEnableVertexArrayAttrib(VAO, 0);
-	glEnableVertexArrayAttrib(VAO, 1);
-	glVertexArrayAttribFormat(VAO, 0, 3, GL_FLOAT, GL_FALSE, 0);
-	glVertexArrayAttribFormat(VAO, 1, 2, GL_FLOAT, GL_FALSE, 3 * sizeof(float));
-	glVertexArrayAttribBinding(VAO, 0, 0);
-	glVertexArrayAttribBinding(VAO, 1, 0);
-
-	//set up framebuffer
-	glClearColor(0.5f, 0.1f, 0.3f, 1.0f);
+	/**
+	 * Setup some basic properties for screen size, background color and point size
+	 */
+	glViewport(0, 0, SCREENX, SCREENY);
+	glClearColor(0.05f, 0.05, 0.05f, 1.0f);
 	glEnable(GL_DEPTH_TEST);
-	glm::mat4 projection_transform = glm::perspective(45.0f, aspectRatio, 0.1f, 10.0f);
-	glUniformMatrix4fv(glGetUniformLocation(ourShader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection_transform));
+	glPointSize(2.0f);
 
-    // render loop
-    // -----------
-    while (!glfwWindowShouldClose(window))
-    {
-        // input
-        // -----
-		processInput(window, playerPosition, playerEulers);
-        // render
-        // ------
+	// Draw Loop
+	while (glfwWindowShouldClose(window) == 0)
+	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 		glfwPollEvents();
 
-		glm::vec3 forwards{
-			glm::sin(glm::radians(playerEulers.y)) * glm::cos(glm::radians(playerEulers.z)),
-			glm::sin(glm::radians(playerEulers.y)) * glm::sin(glm::radians(playerEulers.z)),
-			glm::cos(glm::radians(playerEulers.y))};
-		glm::vec3 globalUp{0.0f, 0.0f, 1.0f};
-		glm::vec3 right{glm::cross(forwards, globalUp)};
-		glm::vec3 up{glm::cross(right, forwards)};
-		glm::mat4 view_transform{glm::lookAt(playerPosition, playerPosition + forwards, up)};
-		glUniformMatrix4fv(glGetUniformLocation(ourShader.ID, "view"), 1, GL_FALSE, glm::value_ptr(view_transform));
+		glUseProgram(computeProgram);
+		glDispatchCompute(NUM_PARTICLES / WORK_GROUP_SIZE, 1, 1);
 
-		// update transform
-		float angle{glm::radians(static_cast<float>(10 * glfwGetTime()))};
-		glm::mat4 model_transform{glm::mat4(1.0f)};
-		model_transform = glm::translate(model_transform, {1.0f, -3.0f, 0.5f});
-		model_transform = glm::rotate(model_transform, angle, {1.0f, 0.0f, 0.0f});
-		model_transform = glm::rotate(model_transform, 2 * angle, {0.0f, 1.0f, 0.0f});
-		glUniformMatrix4fv(glGetUniformLocation(ourShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(model_transform));
-
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		ourShader.use();
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, vertexCount);
+		/**
+		 * Draw the particles
+		 */
+		glUseProgram(shaderProgram);
+		glDrawArraysInstanced(GL_POINTS, 0, 1, NUM_PARTICLES);
 
 		glfwSwapBuffers(window);
-    }
+	}
 
-	glDeleteBuffers(1, &VBO);
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteProgram(ourShader.ID);
+	// OpenGL Shutdown
+	glDeleteProgram(shaderProgram);
+	glDeleteProgram(computeProgram);
+	delete[] vertCode;
+	delete[] fragCode;
+	delete[] computeCode;
+
+	// Window Shutdown
+	glfwDestroyWindow(window);
 	glfwTerminate();
-    return 0;
+	return 0;
 }
