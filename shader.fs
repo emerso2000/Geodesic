64ac6b9d71e53ac93b9b129ff65e5b2408ea4d63
emerso2000 <83967836+emerso2000@users.noreply.github.com>
2023-04-11 17:22:15 -0400

    Update shader.fs

--- shader.fs
+++ shader.fs
@@ -1,8 +1,7 @@
-#version 450 core
-layout (location = 0) in vec2 fragmentTexCoords;
-uniform sampler2D basicTexture;
-out vec4 finalColor;
-void main()
-{
-    finalColor = texture(basicTexture, fragmentTexCoords);
+#version 450
+
+out vec4 outColor;
+
+void main() {
+	outColor = vec4(0.7, 1.0, 1.0, 1.0);
 }
